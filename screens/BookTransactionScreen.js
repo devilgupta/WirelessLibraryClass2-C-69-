9bf061ba90697592cd34c988b1735b69eebf5660
devilgupta <57756025+devilgupta@users.noreply.github.com>
2020-05-26 11:50:01 +0530

    Add files via upload

---
+++ screens/BookTransactionScreen.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import {Text,View, TouchableOpacity,StyleSheet} from 'react-native';
+import * as Permissions from 'expo-permissions';
+import {BarCodeScanner} from 'expo-barcode-scanner';
+
+export default class TransactionScreen extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            hasCameraPermissions:null,
+            scanned:false,
+            scannedData:'',
+            buttonState:'normal'
+        }
+    }
+
+    getCameraPermissions=async()=>{
+        const {status}=await Permissions.askAsync(Permissions.CAMERA);
+        this.setState({
+            hasCameraPermissions:status==="granted"
+        })
+    }
+
+    handleBarCodeScanned=async({type,data})=>{
+        this.setState({
+            scanned:true,
+            scannedData:data,
+            buttonState:'normal'
+        })
+    }
+    render(){
+        const hasCameraPermissions=this.state.hasCameraPermissions;
+        const scanned=this.state.scanned;
+        const buttonState=this.state.buttonState;
+
+        if(buttonState==="clicked" && hasCameraPermissions){
+
+        return(
+            <BarCodeScanner>
+                onBarCodeScanned={scanned?undefined:this.handleBarCodeScanned}
+                style={StyleSheet.absoluteFillObject}
+            </BarCodeScanner>
+        )
+        }
+        else if(buttonState==="normal" ){
+            return(
+            <View style={styles.container}>
+                <Text style={styles.displayText}>
+                    {hasCameraPermissions===true?this.state.scannedData:"Request Camera Permissions"}
+                </Text>
+                <TouchableOpacity 
+                onPress={this.getCameraPermissions}
+                style={styles.scanButton}>
+                    <Text>
+                        Scan QR Code
+                    </Text>
+                </TouchableOpacity>
+            </View>
+            )
+        }
+    }
+}
+
+const styles=StyleSheet.create({
+    container:{
+        flex:1,
+        alignItems:'center',
+        justifyContent:'center'
+    },
+    displayText:{
+        fontSize:15,
+        textDecorationLine:'underline',
+    },
+    scanButton:{
+        backgroundColor:'yellow',
+        padding:10,
+        margin:10
+    }
+})
